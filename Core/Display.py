19dde4b1b5a87e0e9ab9a53d2f5de98e8ccbe9ae
Blake Zimmerman <blakez@sci.utah.edu>
2019-07-30 16:40:10 -0600

    Refined image display and grid display to only work for structured grids

--- Core/Display.py
+++ Core/Display.py
@@ -99,35 +99,33 @@ def ExtractImageSlice(Image, dim, sliceIdx, color):
         return Image(im)
 
 
-def GetAspect(Image, dim='z', color=False, axis='default', retFloat=True):
+def GetAspect(Image, axis='default', retFloat=True):
     '''given a image grid, determines the 2D aspect ratio of the
     off-dimension'''
 
-    if color:
-        imsz = list(Image.data.size()[1:])
-    else:
-        imsz = list(Image.data.size())
+    imsz = Image.size.tolist()
 
-    if color:
-        # aspect is always (displayed) height spacing/width spacing
-        # sz is displayed (displayed, apparent) [width, height]
-        if dim == 'x':
-            aspect = Image.spacing[2]/Image.spacing[0]
-            sz = [imsz[0], imsz[2]]
-        elif dim == 'y':
-            aspect = Image.spacing[1]/Image.spacing[0]
-            sz = [imsz[0], imsz[1]]
-        else:
-            aspect = Image.spacing[1]/Image.spacing[2]
-            sz = [imsz[2], imsz[1]]
-    else:
-        aspect = Image.spacing[1] / Image.spacing[0]
-        sz = [imsz[1], imsz[0]]
+    # if color:
+    #     # aspect is always (displayed) height spacing/width spacing
+    #     # sz is displayed (displayed, apparent) [width, height]
+    #     if dim == 'x':
+    #         aspect = Image.spacing[2]/Image.spacing[0]
+    #         sz = [imsz[0], imsz[2]]
+    #     elif dim == 'y':
+    #         aspect = Image.spacing[1]/Image.spacing[0]
+    #         sz = [imsz[0], imsz[1]]
+    #     else:
+    #         aspect = Image.spacing[1]/Image.spacing[2]
+    #         sz = [imsz[2], imsz[1]]
+    # else:
+
+    # Might need to flip these two
+    aspect = (Image.spacing[1] / Image.spacing[0]).item()
+    sz = [imsz[1], imsz[0]]
 
     if axis == 'cart':
         aspect = 1.0/aspect
         sz = sz[::-1]
-    aspect = aspect.item()  # need to make the aspect not tensors
 
     if retFloat:
         return aspect
@@ -144,12 +142,11 @@ def GetAspect(Image, dim='z', color=False, axis='default', retFloat=True):
     return [int(round(scale[0])), int(round(scale[1]))]
 
 
-
 def DispImage(Image, rng=None, cmap='gray', title=None,
               new_figure=True, color=False, colorbar=True, axis='default',
-              dim='z', sliceIdx=None):
+              dim=0, slice_index=None):
     """
-    :param Image: Input Image ([RGB[A]], [Z], X, Y) or Tensor ([RGB[A]], [Z], X, Y)
+    :param Image: Input Image ([RGB[A]], [Z], Y, X)
     :param rng: Intensity range (list, tuple) (defaults to data intensity limits)
     :param cmap: Matplotlib colormap ('gray', 'jet', etc)
     :param title: Figure Title (string)
@@ -159,181 +156,139 @@ def DispImage(Image, rng=None, cmap='gray', title=None,
                                       and the x direction is vertical
                                  'cart' has (0,0) in the lower left hand corner
                                       and the x direction is horizontal
-    :param dim: Dimension along which to plot ('x', 'y', ['z'])
+    :param dim: Dimension along which to plot ([0], 1, 2)
     :param sliceIdx: Slice index along 'dim' to plot
     :return: None
     """
     import matplotlib.pyplot as plt
     plt.ion()  # tell it to use interactive mode -- see results immediately
 
+    if type(Image).__name__ != 'StructuredGrid':
+        raise RuntimeError(
+            f'Can only plot StructuredGrid types - received {type(Image).__name__}'
+        )
+
+    # Make sure the image is only 2D at this point
+    if len(Image.size) == 3:
+        if not slice_index:
+            sliceIdx = Image.size[0] // 2
+        Image = Image.extract_slice(slice_index, dim)
+
+    # Get the aspect ratio of the image
+    aspect = GetAspect(Image, axis=axis, retFloat=True)
+
     # Define a function to display a tensor
     # This allows us to pass both an Image and a Tensor type into DispImage
-    def _display_tensor(tensor, rng=None, cmap='gray', title=None, new_figure=True, colorbar=True,
-                        axis='default', dim='z', color=False, aspect=[1, 1]):
+    # def _display_tensor(tensor, rng=None, cmap='gray', title=None, new_figure=True, colorbar=True,
+    #                     axis='default', dim='z', color=False, aspect=[1, 1]):
 
-        im = tensor.to('cpu').detach()  # Make sure that the tensor is on the CPU and detached
+    im = Image.data.to('cpu').detach().clone()  # Make sure that the tensor is on the CPU and detached
 
-        # create the figure if requested
-        if new_figure:
-            fig = plt.figure()
-        plt.clf()  # don't be slow, also clear colorbars if necessary
-        if rng is None:
-            mm = [im.min().item(), im.max().item()]
-            vmin = mm[0]
-            vmax = mm[1]
-            if mm[1] - mm[0] == 0:
-                vmin -= 1
-                vmax += 1
-        else:
-            vmin, vmax = rng
+    # create the figure if requested
+    if new_figure:
+        fig = plt.figure()
+    plt.clf()  # don't be slow, also clear colorbars if necessary
+    if rng is None:
+        mm = [im.min().item(), im.max().item()]
+        vmin = mm[0]
+        vmax = mm[1]
+        if mm[1] - mm[0] == 0:
+            vmin -= 1
+            vmax += 1
+    else:
+        vmin, vmax = rng
 
+    # if color:  # for color images
+    #     # color images need to be in 0-1 range
+    #     if im.max(0) > 1.0:
+    #         denom = max(2.0 ** (np.ceil(np.log2(im.max(0)))), 1.0)  # nextpow2
+    #     else:
+    #         denom = 1.0
+    #     imnp = im.numpy() / denom
+    # else:
+    imnp = im.squeeze().numpy()
+
+    # If it is 3D now, then it is color
+
+    if np.isnan(imnp).any():
+        raise DisplayException("DispImage: Image contains NaNs, cannot plot")
+
+    if axis == 'default':
+        img = plt.imshow(imnp, cmap=cmap, vmin=vmin, vmax=vmax, aspect=aspect, origin='lower')
+
+    else:  # axis == 'cart'
         if color:  # for color images
-            # color images need to be in 0-1 range
-            if im.max(0) > 1.0:
-                denom = max(2.0 ** (np.ceil(np.log2(im.max(0)))), 1.0)  # nextpow2
-            else:
-                denom = 1.0
-            imnp = im.numpy() / denom
+            img = plt.imshow(np.squeeze(imnp.transpose(2, 1, 0)),
+                             cmap=cmap, vmin=vmin, vmax=vmax, aspect=aspect)
         else:
-            imnp = im.numpy()
+            img = plt.imshow(np.squeeze(imnp.transpose()),
+                             cmap=cmap, vmin=vmin, vmax=vmax, aspect=aspect)
+        plt.gca().invert_yaxis()
 
-        if np.isnan(imnp).any():
-            raise DisplayException("DispImage: Image contains NANs, cannot plot")
+    plt.xticks([])  # no ticks
+    plt.yticks([])
+    plt.axis('off')  # no border
+    if title is not None:
+        plt.title(title)
+    img.set_interpolation('nearest')
 
-        if axis == 'default':
-            img = plt.imshow(imnp, cmap=cmap, vmin=vmin, vmax=vmax, aspect=aspect)
+    if colorbar:
+        plt.colorbar()
 
-        else:  # axis == 'cart'
-            if color:  # for color images
-                img = plt.imshow(np.squeeze(imnp.transpose(2, 1, 0)),
-                                 cmap=cmap, vmin=vmin, vmax=vmax, aspect=aspect)
-            else:
-                img = plt.imshow(np.squeeze(imnp.transpose()),
-                                 cmap=cmap, vmin=vmin, vmax=vmax, aspect=aspect)
-            plt.gca().invert_yaxis()
-
-        plt.xticks([])  # no ticks
-        plt.yticks([])
-        plt.axis('off')  # no border
-        if title is not None:
-            plt.title(title)
-        img.set_interpolation('nearest')
-
-        if colorbar:
-            plt.colorbar()
-        plt.draw()
-#         plt.show()  # plt.ion() doesn't always work correctly
-        # plt.pause(0.0000001)        # this is bullshit
-        # fig.tight_layout()
-        plt.autoscale()
-
-        return img
-
-    # Check what type was passed and make sure we are working with a tensor
-    if type(Image).__name__ == 'Tensor':
-        im = Image.clone().squeeze()  # Make sure we don't mess with the original tensor
-        aspect = 1.0  # If the input is a tensor, nothing is know about the spacing, so aspect is determined by size
-
-    elif type(Image).__name__ == 'Image':
-        im = Image.data.clone().squeeze()  # Make sure we don't mess with the original tensor
-        aspect = GetAspect(Image, dim=dim, axis=axis, retFloat=True, color=color)
-
-    im = im.to('cpu').detach()  # Make sure that the tensor is on the CPU and detached
-
-    # Use the DispTensor to do most of the work soe we can call this on either an image or a tensor
-    img = _display_tensor(im, rng=rng, cmap=cmap, title=title, new_figure=new_figure,
-                          color=color, colorbar=colorbar, axis=axis, dim=dim, aspect=aspect)
-
-    return img
+    plt.draw()
+    plt.autoscale()
 
 
-def DispHGrid(Field, grid_size=None, title=None,
-              newFig=True, fig_len=7, dim='z', axis=None,
-              slice_index=None):
+# def DisplayJacobianDeterminant(Field, rng=None, cmap='jet', title=None, newFig=True, colorbar=True):
+#
+#     if type(Field).__name__ != 'StructuredGrid':
+#         raise RuntimeError(
+#             f'Can only plot StructuredGrid types - received {type(Field).__name__}'
+#         )
+
+
+def DispFieldGrid(Field, grid_size=None, title=None,
+                  newFig=True, fig_len=7, dim='z', axis=None,
+                  slice_index=None):
     """Displays a grid of the displacement field h
-    Assumed to be ([Z], X, Y, 3)"""
-
-    def _get_info(h, slice_index, dim, Field=None):
-
-        h = h.to('cpu').detach()  # Make sure that the tensor is on the CPU and detached
-
-        if len(h.size()) == 4:
-
-            if dim == 'z':
-                if not slice_index:
-                    slice_index = h.size()[0] // 2
-                h = h[slice_index, :, :].squeeze()
-                hx = h[:, :, 1].numpy()
-                hy = h[:, :, 2].numpy()
-                sx = h.size()[0]
-                sy = h.size()[1]
-                if Field:
-                    spx = Field.spacing()[1].item()
-                    spy = Field.spacing()[2].item()
-            elif dim == 'y':        # not tested
-                if not slice_index:
-                    slice_index = h.size()[2] // 2
-                h = h[:, :, slice_index].squeeze()
-                hx = h[:, :, 0].numpy()
-                hy = h[:, :, 1].numpy()
-                sx = h.size()[0]
-                sy = h.size()[1]
-                if Field:
-                    spx = Field.spacing()[0].item()
-                    spy = Field.spacing()[1].item()
-            elif dim == 'x':        # not tested
-                if not slice_index:
-                    slice_index = h.size()[1] // 2
-                h = h[:, slice_index, :].squeeze()
-                hx = h[:, :, 0].numpy()
-                hy = h[:, :, 2].numpy()
-                sx = h.size()[0]
-                sy = h.size()[1]
-                if Field:
-                    spx = Field.spacing()[0].item()
-                    spy = Field.spacing()[2].item()
-
-        else:
-            hx = h.numpy()[:, :, 0]
-            hy = h.numpy()[:, :, 1]
-            sx = h.size()[0]
-            sy = h.size()[1]
-            if Field:
-                spx = Field.spacing()[0].item()
-                spy = Field.spacing()[1].item()
-
-        if 'spx' not in locals():
-            spx = 1.0
-            spy = 1.0
-
-        return hx, hy, sx, sy, spx, spy
+    Assumed to be ([dim], [Z], Y, X)"""
 
     import matplotlib.pyplot as plt
     plt.ion()  # tell it to use interactive mode -- see results immediately
 
-    # Check what type was passed and make sure we are working with a tensor
-    if type(Field).__name__ == 'Tensor':
-        h = Field.clone()  # Make sure we don't mess with the original tensor
-        hy, hx, sx, sy, spx, spy = _get_info(h, slice_index, dim)  # x,y flipped for plotting (may not always be right)
+    if type(Field).__name__ != 'StructuredGrid':
+        raise RuntimeError(
+            f'Can only plot StructuredGrid types - received {type(Field).__name__}'
+        )
 
-    elif type(Field).__name__ == 'Field':
-        h = Field.h.clone()  # Make sure we don't mess with the original tensor
-        hy, hx, sx, sy, spx, spy = _get_info(h, slice_index, dim, Field) # x,y flipped for plotting
+    # Make sure the image is only 2D at this point
+    if len(Field.size) == 3:
+        if not slice_index:
+            slice_index = Field.size[0] // 2
+        Field = Field.extract_slice(slice_index, dim)
 
-    # account for image spacing
-    hx *= spx
-    hy *= spy
+    # It will be 2D at this point
+    # spx = Field.spacing[-1].item()
+    # spy = Field.spacing[-2].item()
 
-    realsx = (sx - 1) * spx
-    realsy = (sy - 1) * spy
+    field = Field.data.clone()
+    # Change the field to be between -1 and 1
+    field = field - Field.origin.view(*Field.size.shape, *([1] * len(Field.size)))
+    field = field / (Field.spacing * (Field.size / 2)).view(*Field.size.shape, *([1] * len(Field.size)))
+    field = field - 1
 
-    if axis == 'cart':
-        hx, hy = hy.T, hx.T
-    elif axis is not None:
-        raise DisplayException("Unknown value for 'axis'")
+    field_y = field[-1].cpu().detach().squeeze().numpy()  # X Coordinates
+    field_x = field[-2].cpu().detach().squeeze().numpy()  # Y Coordinates
 
-    # create the figure if requested
-    # TODO: Non square grid plots
+    sy = Field.size[-1].item()
+    sx = Field.size[-2].item()
+
+    # hx *= spx # The fields should already be in real space
+    # hy *= spy
+
+    # realsx = (sx - 1) * spx
+    # realsy = (sy - 1) * spy
+    #
     if newFig:
         grid = plt.figure(figsize=(fig_len, fig_len), dpi=140)
         grid.set_facecolor('white')
@@ -350,23 +305,22 @@ def DispHGrid(Field, grid_size=None, title=None,
         grid_sizex = grid_size
         grid_sizey = grid_size
 
+    grid_sizex = int(grid_sizex)
+    grid_sizey = int(grid_sizey)
+
     # This may not always be right
-    hx_sample_h = hx[grid_sizex//2::grid_sizex, :]
-    hy_sample_h = hy[grid_sizex//2::grid_sizex, :]
+    hx_sample_h = field_x[grid_sizex//2::grid_sizex, :]
+    hy_sample_h = field_y[grid_sizex//2::grid_sizex, :]
 
-    hx_sample_v = hx[:, grid_sizey//2::grid_sizey]
-    hy_sample_v = hy[:, grid_sizey//2::grid_sizey]
-
+    hx_sample_v = field_x[:, grid_sizey//2::grid_sizey]
+    hy_sample_v = field_y[:, grid_sizey//2::grid_sizey]
 
     # keep the figure square, but make sure the whole grid fits in the fig
     # These fields should always range between [-1, 1]
     minax = -1
     maxax = 1
 
-    if axis == 'cart':
-        plt.axis([minax, maxax, minax, maxax])
-    else:  # flip y axis
-        plt.axis([minax, maxax, maxax, minax])
+    plt.axis([minax, maxax, maxax, minax])
 
     # plot horizontal lines (y values)
     plt.plot(hy_sample_h.transpose(), hx_sample_h.transpose(), 'k')
@@ -376,7 +330,123 @@ def DispHGrid(Field, grid_size=None, title=None,
     plt.axis('off')
 
     plt.draw()
-#     plt.show()
+
+#     def _get_info(h, slice_index, dim, Field=None):
+#
+#         h = h.to('cpu').detach()  # Make sure that the tensor is on the CPU and detached
+#
+#         if len(h.size()) == 4:
+#
+#             if dim == 'z':
+#                 if not slice_index:
+#                     slice_index = h.size()[0] // 2
+#                 h = h[slice_index, :, :].squeeze()
+#                 hx = h[:, :, 1].numpy()
+#                 hy = h[:, :, 2].numpy()
+#                 sx = h.size()[0]
+#                 sy = h.size()[1]
+#                 if Field:
+#                     spx = Field.spacing()[1].item()
+#                     spy = Field.spacing()[2].item()
+#             elif dim == 'y':        # not tested
+#                 if not slice_index:
+#                     slice_index = h.size()[2] // 2
+#                 h = h[:, :, slice_index].squeeze()
+#                 hx = h[:, :, 0].numpy()
+#                 hy = h[:, :, 1].numpy()
+#                 sx = h.size()[0]
+#                 sy = h.size()[1]
+#                 if Field:
+#                     spx = Field.spacing()[0].item()
+#                     spy = Field.spacing()[1].item()
+#             elif dim == 'x':        # not tested
+#                 if not slice_index:
+#                     slice_index = h.size()[1] // 2
+#                 h = h[:, slice_index, :].squeeze()
+#                 hx = h[:, :, 0].numpy()
+#                 hy = h[:, :, 2].numpy()
+#                 sx = h.size()[0]
+#                 sy = h.size()[1]
+#                 if Field:
+#                     spx = Field.spacing()[0].item()
+#                     spy = Field.spacing()[2].item()
+#
+#         else:
+#             hx = h.numpy()[:, :, 0]
+#             hy = h.numpy()[:, :, 1]
+#             sx = h.size()[0]
+#             sy = h.size()[1]
+#             if Field:
+#                 spx = Field.spacing()[0].item()
+#                 spy = Field.spacing()[1].item()
+#
+#         if 'spx' not in locals():
+#             spx = 1.0
+#             spy = 1.0
+#
+#         return hx, hy, sx, sy, spx, spy
+#
+#     if type(Field).__name__ != 'Stru':
+#         h = Field.h.clone()  # Make sure we don't mess with the original tensor
+#         hy, hx, sx, sy, spx, spy = _get_info(h, slice_index, dim, Field) # x,y flipped for plotting
+#
+#     # account for image spacing
+#     hx *= spx
+#     hy *= spy
+#
+#     realsx = (sx - 1) * spx
+#     realsy = (sy - 1) * spy
+#
+#     if axis == 'cart':
+#         hx, hy = hy.T, hx.T
+#     elif axis is not None:
+#         raise DisplayException("Unknown value for 'axis'")
+#
+#     # create the figure if requested
+#     # TODO: Non square grid plots
+#     if newFig:
+#         grid = plt.figure(figsize=(fig_len, fig_len), dpi=140)
+#         grid.set_facecolor('white')
+#     else:
+#         plt.clf()
+#     if title is not None:
+#         plt.title(title)
+#
+#     # dont allow for more than 127 lines
+#     if grid_size is None:
+#         grid_sizex = max(sx//64, 1)
+#         grid_sizey = max(sy//64, 1)
+#     else:
+#         grid_sizex = grid_size
+#         grid_sizey = grid_size
+#
+#     # This may not always be right
+#     hx_sample_h = hx[grid_sizex//2::grid_sizex, :]
+#     hy_sample_h = hy[grid_sizex//2::grid_sizex, :]
+#
+#     hx_sample_v = hx[:, grid_sizey//2::grid_sizey]
+#     hy_sample_v = hy[:, grid_sizey//2::grid_sizey]
+#
+#
+#     # keep the figure square, but make sure the whole grid fits in the fig
+#     # These fields should always range between [-1, 1]
+#     minax = -1
+#     maxax = 1
+#
+#     if axis == 'cart':
+#         plt.axis([minax, maxax, minax, maxax])
+#     else:  # flip y axis
+#         plt.axis([minax, maxax, maxax, minax])
+#
+#     # plot horizontal lines (y values)
+#     plt.plot(hy_sample_h.transpose(), hx_sample_h.transpose(), 'k')
+#     # plot vertical lines (x values)
+#     plt.plot(hy_sample_v, hx_sample_v, 'k')
+#     # make grid look nicer
+#     plt.axis('off')
+#
+#     plt.draw()
+# #     plt.show()
 
 
 def EnergyPlot(energy, title='Energy', new_figure=True, legend=None):
