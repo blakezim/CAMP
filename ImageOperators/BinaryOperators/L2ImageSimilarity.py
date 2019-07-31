781ee5fe027db1cb5f2dc9c5d2fe83711abb2d41
Blake Zimmerman <blakez@sci.utah.edu>
2019-07-31 14:34:31 -0600

    Improved the create function

--- ImageOperators/BinaryOperators/L2ImageSimilarity.py
+++ ImageOperators/BinaryOperators/L2ImageSimilarity.py
@@ -8,22 +8,21 @@ class L2Similarity(Filter):
     def __init__(self, dim=2, device='cpu', dtype=torch.float32):
         super(L2Similarity, self).__init__()
 
+        self.device = device
+        self.dtype = dtype
         self.gradient_operator = Gradient.Create(dim=dim, device=device, dtype=dtype)
 
     @staticmethod
     def Create(dim=2, device='cpu', dtype=torch.float32):
         filt = L2Similarity(dim, device, dtype)
-        filt = filt.to(device)  # This will move all tensors in the register_buffer
+        filt = filt.to(device)
         filt = filt.type(dtype)
 
-        # Can't add Field and Images to the register buffer, so we need to make sure they are on the right device
+        # Can't add StructuredGrid to the register buffer, so we need to make sure they are on the right device
         for attr, val in filt.__dict__.items():
-            if hasattr(val, 'to'):
-                val.to(device)
-            if hasattr(val, 'to_'):
+            if type(val).__name__ == 'StructuredGrid':
                 val.to_(device)
-            if hasattr(val, 'type'):
-                val.type(dtype)
+                val.to_type_(dtype)
             else:
                 pass
 
