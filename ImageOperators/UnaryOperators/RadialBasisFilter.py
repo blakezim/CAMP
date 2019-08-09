97f32cff6391fc666eb30fdfe813c0cad1fa31cc
Blake Zimmerman <blakez@sci.utah.edu>
2019-08-09 14:45:43 -0600

    Working incompressible with FFT projection

--- ImageOperators/UnaryOperators/RadialBasisFilter.py
+++ ImageOperators/UnaryOperators/RadialBasisFilter.py
@@ -1,10 +1,11 @@
 import torch
 import numbers
+from math import pi
 
 from Core.StructuredGridClass import StructuredGrid
 from ._UnaryFilter import Filter
-from .AffineTransformFilter import AffineTransform
 from .ApplyGridFilter import ApplyGrid
+import numpy as np
 
 
 class RadialBasis(Filter):
@@ -60,39 +61,39 @@ class RadialBasis(Filter):
         def _sigma(x1, x2):
             mat = torch.eye(len(x1), device=self.device)
 
-            if self.incomp:
-
-                diff = x2 - x1
-
-                numerator = ((self.sigma ** 4) * torch.matmul(torch.ones_like(mat) - mat, diff ** 2)) + (self.sigma ** 2)
-
-                denominator = ((self.sigma ** 2) * (diff ** 2).sum() + 1).pow(3.0/2.0)
-
-                double_grad = numerator / denominator * mat
-
-                lap = (numerator / denominator).sum() * mat
-
-                sigma_mat = (self.sigma ** 4).unsqueeze(0).repeat(self.dim, 1)
-
-                off_diag = torch.ger(diff, diff) * (torch.ones_like(mat) - mat) * sigma_mat
-
-                ggt = off_diag + double_grad
-
-                # diff = x2 - x1
-                # exponential = (-(self.sigma ** 2) * (diff ** 2).sum()).exp()
-                #
-                # grad = -2 * (self.sigma ** 2) * diff * exponential
-                # ggt = torch.ger(grad, -2 * (self.sigma ** 2) * diff) + mat * -2 * (self.sigma ** 2) * exponential
-                #
-                # lap = ((-2 * self.sigma ** 2 + 4*(self.sigma ** 4) * (diff ** 2)) * exponential).sum() * mat
-                mat = ggt - lap
-
-            else:
-                diff = x2.float() - x1.float()
-                r = torch.sqrt(1 + (self.sigma * (diff ** 2).sum()))
-                mat = mat * r
-                # r = -1 * (self.sigma ** 2) * ((x2 - x1) ** 2).sum()
-                # mat = mat * r.exp()
+            # if self.incomp:
+            #
+            #     diff = x2 - x1
+            #
+            #     numerator = ((self.sigma ** 4) * torch.matmul(torch.ones_like(mat) - mat, diff ** 2)) + (self.sigma ** 2)
+            #
+            #     denominator = ((self.sigma ** 2) * (diff ** 2).sum() + 1).pow(3.0/2.0)
+            #
+            #     double_grad = numerator / denominator * mat
+            #
+            #     lap = (numerator / denominator).sum() * mat
+            #
+            #     sigma_mat = (self.sigma ** 4).unsqueeze(0).repeat(self.dim, 1)
+            #
+            #     off_diag = torch.ger(diff, diff) * (torch.ones_like(mat) - mat) * sigma_mat
+            #
+            #     ggt = off_diag + double_grad
+            #
+            #     # diff = x2 - x1
+            #     # exponential = (-(self.sigma ** 2) * (diff ** 2).sum()).exp()
+            #     #
+            #     # grad = -2 * (self.sigma ** 2) * diff * exponential
+            #     # ggt = torch.ger(grad, -2 * (self.sigma ** 2) * diff) + mat * -2 * (self.sigma ** 2) * exponential
+            #     #
+            #     # lap = ((-2 * self.sigma ** 2 + 4*(self.sigma ** 4) * (diff ** 2)) * exponential).sum() * mat
+            #     mat = ggt - lap
+            #
+            # else:
+            diff = x2.float() - x1.float()
+            r = torch.sqrt(1 + (self.sigma * (diff ** 2).sum()))
+            mat = mat * r
+            # r = -1 * (self.sigma ** 2) * ((x2 - x1) ** 2).sum()
+            # mat = mat * r.exp()
 
             return mat
 
@@ -114,34 +115,37 @@ class RadialBasis(Filter):
 
     def _solve_affine(self):
 
-        source_landmarks = torch.cat((self.source_landmarks,
-                                      torch.ones(len(self.source_landmarks), 1, device=self.device)), -1).t()
-        target_landmarks = torch.cat((self.target_landmarks,
-                                      torch.ones(len(self.target_landmarks), 1, device=self.device)), -1).t()
+        source_landmarks_centered = self.source_landmarks - self.source_landmarks.mean(0)
+        target_landmarks_centered = self.target_landmarks - self.target_landmarks.mean(0)
 
         # Solve for the transform between the points
         self.affine = torch.matmul(
             torch.matmul(
-                target_landmarks, source_landmarks.t()
+                target_landmarks_centered.t(), source_landmarks_centered
             ),
             torch.matmul(
-                source_landmarks, source_landmarks.t()
+                source_landmarks_centered.t(), source_landmarks_centered
             ).inverse()
         )
 
         if self.incomp:
-            u, _, vt = torch.svd(self.affine[0:self.dim, 0:self.dim])
-            self.affine[0:self.dim, 0:self.dim] = torch.matmul(u, vt)
+            u, _, vt = torch.svd(self.affine)
+            self.affine = torch.matmul(u, vt.t())
 
-        affine_landmarks = torch.matmul(self.affine, source_landmarks)
-        self.affine_landmarks = affine_landmarks.t()[:, 0:self.dim].contiguous()
+        # Solve for the translation
+        self.translation = self.target_landmarks.mean(0) - torch.matmul(self.affine, self.source_landmarks.mean(0).t()).t()
+
+        self.affine_landmarks = torch.matmul(self.affine, self.source_landmarks.t()).t().contiguous()
+        self.affine_landmarks = self.affine_landmarks + self.translation
 
     def _apply_affine(self, x):
 
-        affine = self.affine.inverse()
-
-        a = affine[0:self.dim, 0:self.dim].view([1] * self.dim + [self.dim, self.dim])
-        t = affine[0:self.dim, self.dim]
+        affine = torch.eye(4, device=self.device, dtype=self.dtype)
+        affine[0:3, 0:3] = self.affine
+        affine[0:3, 3] = self.translation
+        affine = affine.inverse()
+        a = affine[0:self.dim, 0:self.dim]
+        t = affine[-0:self.dim, self.dim]
 
         x.data = torch.matmul(a.unsqueeze(0).unsqueeze(0),
                               x.data.permute(list(range(1, self.dim + 1)) + [0]).unsqueeze(-1))
@@ -162,81 +166,70 @@ class RadialBasis(Filter):
             point = self.target_landmarks[i].view([self.dim] + self.dim * [1]).float()
             weight = self.params[i].view([self.dim] + self.dim * [1]).float()
 
-            if self.incomp:
+            sigma = self.sigma.view([self.dim] + [1]*self.dim)
 
-                diff = (temp.data - point).permute(list(range(1, self.dim + 1)) + [0])
+            temp.data = torch.sqrt(1 + sigma * ((temp - point) ** 2).data.sum(0))
 
-                # ones = torch.ones(list(diff.data.shape) + [self.dim], device=self.device, dtype=self.dtype)
-                # eyes = torch.diag_embed(torch.ones_like(diff))
-                ones = torch.ones([1] * self.dim + [self.dim, self.dim], device=self.device, dtype=self.dtype)
-                eyes = torch.eye(self.dim, device=self.device, dtype=self.dtype).view([1] * self.dim + [self.dim, self.dim])
-                # off_diag = ones - eyes
-                double_grad = (self.sigma ** 4) * torch.matmul(ones - eyes, (diff ** 2).unsqueeze(-1)).squeeze(-1) + (self.sigma ** 2)
+            temp_zeros.data = temp_zeros.data + temp.data * weight
 
-                double_grad = double_grad / ((self.sigma ** 2) * (diff ** 2).sum(-1, keepdim=True) + 1).pow(3.0 / 2.0)
+        if self.incomp:
+            # Expand the size and spacing variable so they are easily usable
+            size = x.size.view(*x.size.shape, *([1] * len(x.size)))
+            spacing = x.spacing.view(*x.spacing.shape, *([1] * len(x.spacing)))
 
-                # double_grad = torch.matmul(eyes, double_grad.unsqueeze(-1)).squeeze(-1)
+            # Create the vectors to mesh grid
+            vecs = [torch.arange(0, x, device=x.device).float() for x in x.size]
 
-                # del num, denom
-                # torch.cuda.empty_cache()
+            # Create the grids from the vectors and stack them in the first dimension
+            grids = torch.meshgrid(vecs)
+            grids = torch.stack(grids, 0)
 
-                lap = eyes * double_grad.sum(-1, keepdim=True).unsqueeze(-1)
+            # Compute the cosine grids
+            # cos_grids = (2 * torch.cos((2.0 * pi * grids) / size) - 2) / (spacing * spacing)
+            sin_grids = torch.sin((2 * pi * grids) / size) / spacing
 
-                ggt = torch.matmul(diff.unsqueeze(-1), diff.unsqueeze(-2)).squeeze() * (ones - eyes)
+            # Make the spatial dimensions linear
+            # cos_grids = cos_grids.view(len(x.size), -1)
+            sin_grids = sin_grids.view(len(x.size), -1)
+            nsq = (sin_grids ** 2).sum(0)
 
-                ggt = ggt * self.sigma.unsqueeze(0).repeat(self.dim, 1).view([1]*self.dim + [self.dim, self.dim])
+            sin_grids = sin_grids.permute(1, 0)
 
-                ggt = ggt + torch.diag_embed(double_grad)
+            # Take the fourier transform of the data
+            fft = torch.rfft(temp_zeros.data, signal_ndim=self.dim, normalized=False, onesided=False)
+            real, imag = fft.split(1, -1)
 
-                mat = ggt - lap
+            # We need to linearize the matrices so we can easily do the multiplication
+            real = real.squeeze().view(len(x.size), -1).permute(1, 0)
+            imag = imag.squeeze().view(len(x.size), -1).permute(1, 0)
 
-                mat = torch.matmul(mat, weight.permute(list(range(1, self.dim + 1)) + [0]).unsqueeze(-1)).squeeze()
+            real_dot = (real * sin_grids).sum(-1, keepdim=True)
+            imag_dot = (imag * sin_grids).sum(-1, keepdim=True)
 
-                temp.data = mat.permute([-1] + list(range(0, self.dim)))
+            real_app = real - (real_dot * sin_grids / nsq.unsqueeze(-1))
+            imag_app = imag - (imag_dot * sin_grids / nsq.unsqueeze(-1))
 
-                temp_zeros.data = temp_zeros.data + temp.data
+            # Deal with the nan
+            real_app[real_app != real_app] = real.squeeze()[real_app != real_app]
+            imag_app[imag_app != imag_app] = imag.squeeze()[imag_app != imag_app]
 
-                rbf_grid.data = rbf_grid.data + temp.data
-                #
-                # exponential = (-(self.sigma ** 2) * (diff ** 2).sum(-1, keepdim=True)).exp()
-                #
-                # grad = -2 * (self.sigma ** 2) * diff * exponential #.unsqueeze(-1)
-                # grad = torch.matmul(grad.unsqueeze(-1), (-2 * (self.sigma ** 2) * diff).unsqueeze(-2))
-                # # ggt = grad + torch.diag_embed((-2 * (self.sigma ** 2) * exponential).unsqueeze(-1).repeat([1] * self.dim + [self.dim]))
-                # ggt = grad + torch.diag_embed((-2 * (self.sigma ** 2) * exponential))
-                #
-                # # lap = ((-2 * self.sigma ** 2 + 4 * (self.sigma ** 4) * (diff ** 2)) * exponential.unsqueeze(-1)).sum(-1)
-                # lap = ((-2 * self.sigma ** 2 + 4 * (self.sigma ** 4) * (diff ** 2)) * exponential).sum(-1)
-                # lap = torch.diag_embed(lap.unsqueeze(-1).repeat([1] * self.dim + [self.dim]))
-                #
-                # mat = ggt - lap
-                #
-                # # Scale by the weights
-                # mat = torch.matmul(mat, weight.permute(list(range(1, self.dim + 1)) + [0]).unsqueeze(-1)).squeeze()
-                #
-                # temp.data = mat.permute([-1] + list(range(0, self.dim)))
-                #
-                # rbf_grid.data = rbf_grid.data + temp.data
+            # Return tensors to their actual shapes
+            real_app = real_app.permute(1, 0).view(len(x.size), *x.size.long())
+            imag_app = imag_app.permute(1, 0).view(len(x.size), *x.size.long())
+            smooth_fft = torch.stack((real_app, imag_app), -1)
 
-            else:
-                sigma = self.sigma.view([self.dim] + [1]*self.dim)
-                # temp.data = -1 * (sigma ** 2) * ((temp - point) ** 2).data.sum(0)
-                # temp.data = torch.exp(temp.data)
+            # Inverse fourier transform
+            temp_zeros.data = torch.irfft(smooth_fft, signal_ndim=self.dim, normalized=False, onesided=False)
 
-                temp.data = torch.sqrt(1 + sigma * ((temp - point) ** 2).data.sum(0))
+            del sin_grids, real_dot, real_app, imag_app, imag_dot
 
-                rbf_grid.data = rbf_grid.data + temp.data * weight
-
-        # temp.set_to_identity_lut_()
-
-        # del temp
-        # torch.cuda.empty_cache()
-        #
-        from ImageOperators.UnaryOperators.JacobianDeterminantFilter import JacobianDeterminant
-        jacobian = JacobianDeterminant.Create(dim=2, device=self.device, dtype=self.dtype)
-        test = jacobian(rbf_grid)
+        rbf_grid.data = rbf_grid.data + temp_zeros.data
 
         rbf_grid = self._apply_affine(rbf_grid)
+
+        from ImageOperators.UnaryOperators.JacobianDeterminantFilter import JacobianDeterminant
+        jacobian = JacobianDeterminant.Create(dim=3, device=self.device, dtype=self.dtype)
+        test = jacobian(rbf_grid)
 
         x_rbf = ApplyGrid.Create(rbf_grid, device=x.device, dtype=x.dtype)(x)
 
