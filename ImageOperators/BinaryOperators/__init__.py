e32e899ea10deae100240b2fbd6233ddc5a6c020
Blake Zimmerman <blakez@sci.utah.edu>
2019-08-09 16:00:40 -0600

    Updates to similarities

--- ImageOperators/BinaryOperators/__init__.py
+++ ImageOperators/BinaryOperators/__init__.py
@@ -1,1 +1,6 @@
-__all__ = []
+from ImageOperators.BinaryOperators.L2ImageSimilarity import *
+from ImageOperators.BinaryOperators.NormalizedCrossCorrelationFilter import *
+__all__ = [
+    'L2Similarity',
+    'NormalizedCrossCorrelation'
+]
