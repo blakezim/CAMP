4fedab46dac2ba7ec7214b3351a944b9421db202
Blake Zimmerman <blakez@sci.utah.edu>
2019-08-13 16:33:06 -0600

    working version of iterative RBF incompressible:

--- ImageOperators/UnaryOperators/RadialBasisFilter.py
+++ ImageOperators/UnaryOperators/RadialBasisFilter.py
@@ -1,18 +1,19 @@
 import torch
 import numbers
-from math import pi
+import torch.nn.functional as F
 
 from CAMP.Core.StructuredGridClass import StructuredGrid
+from CAMP.ImageOperators.BinaryOperators import ComposeGrids
 from ._UnaryFilter import Filter
 from .ApplyGridFilter import ApplyGrid
+from .FluidKernelFilter import FluidKernel
 
 
 # TODO Need a way to store the grid so it can be applied to other volumes
 
 
 class RadialBasis(Filter):
-    def __init__(self, target_landmarks, source_landmarks, sigma=0.001, incompressible=False,
-                 device='cpu', dtype=torch.float32):
+    def __init__(self, target_landmarks, source_landmarks, sigma=0.001, device='cpu', dtype=torch.float32):
         super(RadialBasis, self).__init__()
 
         if target_landmarks.shape != source_landmarks.shape:
@@ -25,27 +26,36 @@ class RadialBasis(Filter):
         self.dtype = dtype
         self.num_landmarks = len(source_landmarks)
         self.dim = len(source_landmarks[0])
+        self.rbf = None
 
         if isinstance(sigma, numbers.Number):
-            self.sigma = torch.as_tensor([sigma] * self.dim)
+            sigma = torch.as_tensor([sigma] * self.dim)
         else:
-            self.sigma = torch.as_tensor(sigma)
+            sigma = torch.as_tensor(sigma)
 
-        self.sigma = self.sigma.to(device)
-        self.sigma = self.sigma.type(dtype)
+        self.register_buffer('sigma', sigma)
+        # self.sigma = self.sigma.to(device)
+        # self.sigma = self.sigma.type(dtype)
 
-        self.source_landmarks = source_landmarks
-        self.target_landmarks = target_landmarks
-        self.incomp = incompressible
+        # self.source_landmarks = source_landmarks
+        # self.target_landmarks = target_landmarks
 
-        self._solve_affine()
-
-        self._solve_matrix_spline()
+        self.register_buffer('source_landmarks', source_landmarks)
+        self.register_buffer('target_landmarks', target_landmarks)
+        # self.incomp = incompressible
+        #
+        # if incompressible:
+        #     self.operator = FluidKernel.Create(
+        #         accu,
+        #         device=self.device,
+        #         alpha=1.0,
+        #         beta=0.0,
+        #         gamma=0.001,
+        #     )
 
     @staticmethod
-    def Create(target_landmarks, source_landmarks, sigma=0.01, incompressible=False,
-               device='cpu', dtype=torch.float32):
-        rbf = RadialBasis(target_landmarks, source_landmarks, sigma, incompressible, device, dtype)
+    def Create(target_landmarks, source_landmarks, sigma=0.01, device='cpu', dtype=torch.float32):
+        rbf = RadialBasis(target_landmarks, source_landmarks, sigma, device, dtype)
         rbf = rbf.to(device)
         rbf = rbf.type(dtype)
 
@@ -57,14 +67,25 @@ class RadialBasis(Filter):
             else:
                 pass
 
+        rbf._solve_affine()
+        rbf._solve_matrix_spline()
+
         return rbf
 
     def _solve_matrix_spline(self):
         def _sigma(x1, x2):
             mat = torch.eye(len(x1), device=self.device)
 
-            diff = x2.float() - x1.float()
-            r = torch.sqrt(1 + (self.sigma * (diff ** 2).sum()))
+            diff = (x2.float() - x1.float())
+            # eps = torch.tensor(1e-9, device=self.device, dtype=self.dtype)
+            # r = diff * torch.log(torch.max(torch.sqrt(diff), eps))
+
+            # r = torch.sqrt(1 + (self.sigma * (diff ** 2).sum()))
+            if self.rbf == 'gaussian':
+                r = (-1 * (self.sigma ** 2) * ((x1 - x2) ** 2).sum()).exp()
+
+            else:
+                r = torch.sqrt(1 + (self.sigma * (diff ** 2).sum()))
             mat = mat * r
             return mat
 
@@ -79,18 +100,18 @@ class RadialBasis(Filter):
                     b[(j * dim):(j * dim) + dim, (i * dim):(i * dim) + dim] = b[(i * dim):(i * dim) + dim,
                                                                               (j * dim):(j * dim) + dim]
 
-        c = (self.affine_landmarks - self.target_landmarks).view(-1)
+        c = (self.tform_landmarks - self.target_landmarks).view(-1)
         # c = self.affine_landmarks.view(-1)
         x = torch.matmul(b.inverse(), c)
         self.params = x.view(self.num_landmarks, dim)
 
-    def _solve_affine(self):
+    def _solve_affine(self, rigid=False):
 
         source_landmarks_centered = self.source_landmarks - self.source_landmarks.mean(0)
         target_landmarks_centered = self.target_landmarks - self.target_landmarks.mean(0)
 
         # Solve for the transform between the points
-        self.affine = torch.matmul(
+        self.tform = torch.matmul(
             torch.matmul(
                 target_landmarks_centered.t(), source_landmarks_centered
             ),
@@ -99,22 +120,26 @@ class RadialBasis(Filter):
             ).inverse()
         )
 
-        if self.incomp:
-            u, _, vt = torch.svd(self.affine)
-            self.affine = torch.matmul(u, vt.t())
+        if rigid:
+            u, _, vt = torch.svd(self.tform)
+            self.tform = torch.matmul(u, vt.t())
 
         # Solve for the translation
-        self.translation = self.target_landmarks.mean(0) - torch.matmul(self.affine,
+        self.translation = self.target_landmarks.mean(0) - torch.matmul(self.tform,
                                                                         self.source_landmarks.mean(0).t()).t()
 
-        self.affine_landmarks = torch.matmul(self.affine, self.source_landmarks.t()).t().contiguous()
-        self.affine_landmarks = self.affine_landmarks + self.translation
+        self.tform_landmarks = torch.matmul(self.tform, self.source_landmarks.t()).t().contiguous()
+        self.tform_landmarks = self.tform_landmarks + self.translation
+
+        # self.rigid_landmarks = torch.matmul(self.rigid, self.source_landmarks.t()).t().contiguous()
+        # self.rigid_landmarks = self.rigid_landmarks + self.translation
 
     def _apply_affine(self, x):
 
         affine = torch.eye(4, device=self.device, dtype=self.dtype)
-        affine[0:3, 0:3] = self.affine
-        affine[0:3, 3] = self.translation
+
+        affine[0:self.dim, 0:self.dim] = self.tform
+        affine[0:self.dim, self.dim] = self.translation
         affine = affine.inverse()
         a = affine[0:self.dim, 0:self.dim]
         t = affine[-0:self.dim, self.dim]
@@ -125,15 +150,28 @@ class RadialBasis(Filter):
 
         return x
 
-    def forward(self, in_grid, out_grid=None):
+    def _sample_coords(self, field):
+        # We are given a vector field
+        out_points = []
+        for point in self.target_landmarks:
+            # Point is in real space
 
-        if out_grid is not None:
-            x = out_grid.clone()
-        else:
-            x = in_grid.clone()
+            # Given point is in z, y, x
+            index_point = (point - field.origin)/field.spacing
+            torch_point = torch.as_tensor((index_point / (field.size / 2) - 1), device=self.device, dtype=self.dtype)
 
-        rbf_grid = StructuredGrid.FromGrid(x, channels=self.dim)
-        rbf_grid.set_to_identity_lut_()
+            # We turn it into a torch coordinat, but now grid sample is expecting x, y, z
+            torch_point = torch.as_tensor((torch_point.tolist()[::-1]), device=self.device, dtype=self.dtype)
+            out_point = F.grid_sample(field.data.unsqueeze(0), torch_point.view([1] * (self.dim + 1) + [self.dim]))
+            out_points.append(out_point.squeeze())
+
+            # Returned point should be z, y, x because that is what is stored in the lut
+
+        out_points = torch.stack(out_points, 0)
+        return out_points
+
+    def _solve_vector_field(self, x):
+
         temp = StructuredGrid.FromGrid(x, channels=self.dim)
         accu = temp.clone() * 0.0
 
@@ -145,66 +183,151 @@ class RadialBasis(Filter):
 
             sigma = self.sigma.view([self.dim] + [1] * self.dim)
 
-            temp.data = torch.sqrt(1 + sigma * ((temp - point) ** 2).data.sum(0))
+            # eps = torch.tensor(1e-9, device=self.device, dtype=self.dtype)
+            # diff = ((temp - point) ** 2).data
+            # temp.data = diff * torch.log(torch.max(diff.sqrt(), eps))
+
+            if self.rbf == 'gaussian':
+                temp.data = -1 * (sigma ** 2) * ((temp - point) ** 2).data.sum(0, keepdim=True)
+                temp.data = torch.exp(temp.data)
+
+            else:
+                temp.data = torch.sqrt(1 + sigma * ((temp - point) ** 2).data.sum(0))
 
             accu.data = accu.data + temp.data * weight
 
-        if self.incomp:
-            # Expand the size and spacing variable so they are easily usable
-            size = x.size.view(*x.size.shape, *([1] * len(x.size)))
-            spacing = x.spacing.view(*x.spacing.shape, *([1] * len(x.spacing)))
+        return accu
 
-            # Create the vectors to mesh grid
-            vecs = [torch.arange(0, x, device=x.device).float() for x in x.size]
-
-            # Create the grids from the vectors and stack them in the first dimension
-            grids = torch.meshgrid(vecs)
-            grids = torch.stack(grids, 0)
-
-            # Compute the cosine grids
-            # cos_grids = (2 * torch.cos((2.0 * pi * grids) / size) - 2) / (spacing * spacing)
-            sin_grids = torch.sin((2 * pi * grids) / size) / spacing
-
-            # Make the spatial dimensions linear
-            # cos_grids = cos_grids.view(len(x.size), -1)
-            sin_grids = sin_grids.view(len(x.size), -1)
-            nsq = (sin_grids ** 2).sum(0)
-
-            sin_grids = sin_grids.permute(1, 0)
-
-            # Take the fourier transform of the data
-            fft = torch.rfft(accu.data, signal_ndim=self.dim, normalized=False, onesided=False)
-            real, imag = fft.split(1, -1)
-
-            # We need to linearize the matrices so we can easily do the multiplication
-            real = real.squeeze().view(len(x.size), -1).permute(1, 0)
-            imag = imag.squeeze().view(len(x.size), -1).permute(1, 0)
-
-            real_dot = (real * sin_grids).sum(-1, keepdim=True)
-            imag_dot = (imag * sin_grids).sum(-1, keepdim=True)
-
-            real_app = real - (real_dot * sin_grids / nsq.unsqueeze(-1))
-            imag_app = imag - (imag_dot * sin_grids / nsq.unsqueeze(-1))
-
-            # Deal with the nan
-            real_app[real_app != real_app] = real.squeeze()[real_app != real_app]
-            imag_app[imag_app != imag_app] = imag.squeeze()[imag_app != imag_app]
-
-            # Return tensors to their actual shapes
-            real_app = real_app.permute(1, 0).view(len(x.size), *x.size.long())
-            imag_app = imag_app.permute(1, 0).view(len(x.size), *x.size.long())
-            smooth_fft = torch.stack((real_app, imag_app), -1)
-
-            # Inverse fourier transform
-            accu.data = torch.irfft(smooth_fft, signal_ndim=self.dim, normalized=False, onesided=False)
-
-            del sin_grids, real_dot, real_app, imag_app, imag_dot
-            torch.cuda.empty_cache()
-
-        rbf_grid.data = rbf_grid.data + accu.data
-
+    def _apply_vector_field(self, in_grid, vec_field):
+        rbf_grid = StructuredGrid.FromGrid(vec_field, channels=self.dim)
+        rbf_grid.set_to_identity_lut_()
+        rbf_grid.data = rbf_grid.data + vec_field.data
         rbf_grid = self._apply_affine(rbf_grid)
+        rbf_image = ApplyGrid.Create(rbf_grid, device=vec_field.device, dtype=vec_field.dtype)(in_grid, vec_field)
 
-        x_rbf = ApplyGrid.Create(rbf_grid, device=x.device, dtype=x.dtype)(in_grid, out_grid)
+        return rbf_image, rbf_grid
 
-        return x_rbf, rbf_grid
+    def forward(self, in_grid, out_grid=None, apply=True):
+
+        if out_grid is not None:
+            x = out_grid.clone()
+        else:
+            x = in_grid.clone()
+
+        # rbf_grid = StructuredGrid.FromGrid(x, channels=self.dim)
+        # rbf_grid.set_to_identity_lut_()
+        accu = self._solve_vector_field(x)
+
+        if apply:
+            return self._apply_vector_field(in_grid, accu)
+        else:
+            return accu
+        # temp = StructuredGrid.FromGrid(x, channels=self.dim)
+        # accu = temp.clone() * 0.0
+        #
+        # for i in range(self.num_landmarks):
+        #     temp.set_to_identity_lut_()
+        #
+        #     point = self.target_landmarks[i].view([self.dim] + self.dim * [1]).float()
+        #     weight = self.params[i].view([self.dim] + self.dim * [1]).float()
+        #
+        #     sigma = self.sigma.view([self.dim] + [1] * self.dim)
+        #
+        #     # eps = torch.tensor(1e-9, device=self.device, dtype=self.dtype)
+        #     # diff = ((temp - point) ** 2).data
+        #     # temp.data = diff * torch.log(torch.max(diff.sqrt(), eps))
+        #
+        #     if self.rbf == 'gaussian':
+        #         temp.data = -1 * (sigma ** 2) * ((temp - point) ** 2).data.sum(0, keepdim=True)
+        #         temp.data = torch.exp(temp.data)
+        #
+        #     else:
+        #         temp.data = torch.sqrt(1 + sigma * ((temp - point) ** 2).data.sum(0))
+        #
+        #     accu.data = accu.data + temp.data * weight
+
+        # if self.incomp:
+        #
+        #     operator = FluidKernel.Create(
+        #         accu,
+        #         device=self.device,
+        #         alpha=1.0,
+        #         beta=0.0,
+        #         gamma=0.001,
+        #     )
+        #
+        #     # Project the deformation into incompressible
+        #     accu = operator.project_incompressible(accu)
+
+            # This field likely no longer matches the points very well
+            # Look up the target points and compare them to the source points
+        # def_landmarks = self._sample_coords(accu)
+        # diff = def_landmarks - self.affine_landmarks
+
+        # if apply:
+        #     rbf_grid.data = rbf_grid.data + accu.data
+        #     rbf_grid = self._apply_affine(rbf_grid)
+        #     x_rbf = ApplyGrid.Create(rbf_grid, device=x.device, dtype=x.dtype)(in_grid, out_grid)
+        #     return x_rbf, rbf_grid
+        #
+        # else:
+        #     return accu
+
+    def filter_incompressible(self, in_grid, out_grid, t_step=10):
+
+        # Store the target landmarks as we are going to change these
+        target_landmarks_original = self.target_landmarks.clone()
+
+        # Need to recompute the parameters with the rigid
+        self._solve_affine(rigid=True)
+        self._solve_matrix_spline()
+
+        operator = FluidKernel.Create(
+            out_grid,
+            device=self.device,
+            alpha=1.0,
+            beta=0.0,
+            gamma=0.001,
+        )
+
+        composer = ComposeGrids.Create(device=self.device, dtype=self.dtype)
+
+        # TODO Add some sort of convergence detection
+        # step=0.5
+
+        accu = StructuredGrid.FromGrid(out_grid, channels=self.dim)
+        accu.set_to_identity_lut_()
+        id = accu.copy()
+
+        for _ in range(0, t_step):
+
+            # Do the forward
+            def_vec = self.forward(in_grid, out_grid, apply=False)  # Get the vector field
+            def_vec = operator.project_incompressible(def_vec)  # Project the vector field to divergent free
+            # Compose the field
+            accu = composer([accu, id + def_vec])
+
+            self.target_landmarks = target_landmarks_original.clone()
+            def_landmarks = self._sample_coords(accu)  # Sample the grid at the target points
+            self.target_landmarks = def_landmarks.clone()  # Make the new target points the sampled points
+            self._solve_matrix_spline()
+            print(torch.norm(def_landmarks - self.tform_landmarks, p=2, dim=1).sum())
+
+        # for _ in range(0, t_step - 1):
+        #
+        #     # NEED TO RESOLVE PARAMETERS
+        #     self._solve_matrix_spline()
+        #     temp_vec = self.forward(in_grid, out_grid, apply=False)
+        #     temp_vec = operator.project_incompressible(temp_vec)
+        #     # Need to figure out how to add it to the last one
+        #     def_vec = def_vec + step*temp_vec
+        #     # Return the landmarks to how they were so the vector field compare with the right points
+        #     # self.target_landmarks = target_landmarks_original.clone()
+        #     def_landmarks = self._sample_coords(temp_vec)
+        #     self.target_landmarks = def_landmarks.clone()
+        #     print(torch.norm(def_landmarks - self.tform_landmarks, p=2, dim=1).sum())
+
+        # Return the landmarks to how they were
+        self.target_landmarks = target_landmarks_original.clone()
+
+        return self._apply_vector_field(in_grid, accu - id)
