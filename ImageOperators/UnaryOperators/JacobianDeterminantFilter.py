39dc56a2c60d15593beb21d7312446189bc89ae7
Blake Zimmerman <blakez@sci.utah.edu>
2019-08-13 08:12:28 -0600

    Changed to crop by 1 and pad with ones for edge case

--- ImageOperators/UnaryOperators/JacobianDeterminantFilter.py
+++ ImageOperators/UnaryOperators/JacobianDeterminantFilter.py
@@ -1,4 +1,6 @@
 import torch
+import itertools
+import torch.nn.functional as F
 
 from CAMP.Core.StructuredGridClass import StructuredGrid
 from .GradientFilter import Gradient
@@ -13,6 +15,16 @@ class JacobianDeterminant(Filter):
         self.device = device
         self.dtype = dtype
         self.gradient = Gradient(dim=dim)
+        self.pad = F.pad
+        self.pad_vec = []
+        self.pad_vec += [[1, 1] for _ in range(0, dim)]
+        self.pad_vec = tuple(itertools.chain.from_iterable(self.pad_vec))
+
+        self.crop_vec = []
+        self.crop_vec += [[-1, -1] for _ in range(0, dim)]
+        self.crop_vec = tuple(itertools.chain.from_iterable(self.crop_vec))
+
+
 
     @staticmethod
     def Create(dim=2, device='cpu', dtype=torch.float32):
@@ -32,8 +44,17 @@ class JacobianDeterminant(Filter):
 
     def forward(self, x):
 
-        # mat = torch.eye(self.dim, device=self.device, dtype=self.dtype)
-        grads = self.gradient(x / (x.spacing.view([self.dim] + [1]*self.dim)))
+        # # Have to pad the tensor with the correct values otherwise edges will be huge
+        # temp = StructuredGrid(
+        #     size=x.size + 2,
+        #     spacing=x.spacing,
+        #     origin=(x.origin - x.spacing),
+        #     device=x.device,
+        #     dtype=x.dtype,
+        # )
+        # temp.set_to_identity_lut_()
+        # temp[]
+        grads = self.gradient(x)
         # Central difference scale the gradients by the spacing
         # grads = grads / 2.0
 
@@ -43,6 +64,9 @@ class JacobianDeterminant(Filter):
             det = grads[0] * (grads[4] * grads[8] - grads[5] * grads[7]) - \
                   grads[1] * (grads[3] * grads[8] - grads[5] * grads[6]) + \
                   grads[2] * (grads[3] * grads[7] - grads[4] * grads[6])
+
+        # Crop the output and then pad with ones
+        det = self.pad(self.pad(det, pad=self.crop_vec), pad=self.pad_vec, mode='constant', value=1.0)
 
         out = StructuredGrid.FromGrid(
             x,
